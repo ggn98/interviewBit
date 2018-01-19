97254e6885294a3ce6e4b3206f4741fbd3219b9b
pro98 <35041205+pro98@users.noreply.github.com>
2018-01-20 01:37:41 +0530

    sorted insert position

---
+++ Binary Search/sortedInsertPosition.cpp
@@ -0,0 +1,25 @@
+int Solution::searchInsert(vector<int> &A, int B) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int start=0,end=A.size()-1,ans=-1;
+    if(B<A[0])
+    return 0;
+    if(B>A[end])
+    return A.size();
+    while(start<=end)
+    {
+        int mid=(start+end)/2;
+        if(A[mid]>=B)
+        {
+            ans=mid;
+            end=mid-1;
+        }
+        else
+        {
+            start=mid+1;
+        }
+    }
+    return ans;
+}
