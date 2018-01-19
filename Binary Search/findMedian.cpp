616cc31568bc26815489b4150893ed3ed8db0bba
pro98 <35041205+pro98@users.noreply.github.com>
2018-01-19 13:35:49 +0530

    Update findMedian.cpp

--- Binary Search/findMedian.cpp
+++ Binary Search/findMedian.cpp
@@ -14,21 +14,21 @@ int Solution::findMedian(vector<vector<int> > &A)
             max=A[i][columns-1];
         }
     }
-    int totalNoToBeSmallerThanMid=(1+rows*columns)/2; //number equal to the median and less than median
-    while(min<max)
+    int totalNoToBeSmallerThanMid=(1+rows*columns)/2; //number of elements equal to the median and less than median
+    while(min<max) // while(min!=max)
     {
         int mid=(min+max)/2;
         int count=0;
         for(int i=0;i<rows;i++)
         {
-            count+=upper_bound(A[i].begin(),A[i].end(),mid)-A[i].begin();
+            count+=upper_bound(A[i].begin(),A[i].end(),mid)-A[i].begin();//gives count of elements maller than equal to mid in each row
         }
         if(count<totalNoToBeSmallerThanMid)
         min=mid+1;
         else // the mid which satisfies, may or may not be present in matrix ,hence we do not return direectly
-        max=mid; //instead we update max to mid
+        max=mid; //instead we update max to mid, min or max returned finally would always be present in the matrix
     }
-    //return min or max , as this return statement is executed when min=max
+    //return min or max , as this return statement is executed when min=max , in each case min=max , as ans exists always
     return max; // or return min
 }
 // https://www.youtube.com/watch?v=BpETOGNqKmo -> for full explaination , thanks to this guy ! :)
