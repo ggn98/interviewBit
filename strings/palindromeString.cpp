0a9bed32e26d2f56f97022fe53a88cca4be07aa5
pro98 <35041205+pro98@users.noreply.github.com>
2018-01-24 00:50:10 +0530

    String palindrome

---
+++ strings/palindromeString.cpp
@@ -0,0 +1,45 @@
+bool isAlphaNum(char ch)
+{
+    if((ch>='1'&&ch<='9')||(ch>='A'&&ch<='Z')||(ch>='a'&&ch<='z'))
+    return true;
+    return false;
+}
+int Solution::isPalindrome(string A) 
+{
+    int start=0,end=A.size()-1;
+    while(start<=end)
+    {
+        if(isAlphaNum(A[start])==false)
+        {
+            start++;
+        }
+        if(isAlphaNum(A[end])==false)
+        {
+            end--;
+        }
+        if(isAlphaNum(A[start])&&isAlphaNum(A[end]))
+        {
+            if(abs(A[start]-A[end])==32||A[start]-A[end]==0)
+            {
+                start++;
+                end--;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else 
+        {
+            if(isAlphaNum(A[start])==false)
+            {
+                 start++;
+            }
+        if(isAlphaNum(A[end])==false)
+            {
+                end--;
+            }
+        }
+    }
+    return 1;
+}
