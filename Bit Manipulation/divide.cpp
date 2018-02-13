47df8cc138d17428ac7cc3612e35b59b3d430a00
pro98 <gagannkishorupadhyay@gmai.com>
2018-02-14 00:13:28 +0530

    Division using bitwise

--- Bit Manipulation/divide.cpp
+++ Bit Manipulation/divide.cpp
@@ -16,11 +16,7 @@ int Solution::divide(int numerator, int denominator)
             temp=temp<<1;
             multiple=multiple<<1;
         }
-        //temp is largest power of 2 and multiple of denominator , which is less than numerator
-        //ex for 50/4 in loop 1 , temp=32 (largest power of 2 and multiple of 4 , less than 50)
-        //hence update num to num-temp i.e here 50-32=18 , in step 1
-        //add the multiple to res
-        //continue this till numerator is less than denominator
+        //temp = multiple * denominator
         num=num-temp; 
         res+=multiple;
     }
