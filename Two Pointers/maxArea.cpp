1aa731136264282b139869563a35bf0a77355c7b
pro98 <gagannkishorupadhyay@gmai.com>
2018-03-23 00:29:48 +0530

    Water container problem

--- Two Pointers/maxArea.cpp
+++ Two Pointers/maxArea.cpp
@@ -14,6 +14,7 @@ int Solution::maxArea(vector<int> &A)
 }
 //WE have area set initially as min(A[0],A[n-1]) * (n-1)
 //if we want to maximise this area 
-//then for sure base of that triangle will be less than this , and hence height would be surely more th
-//A[0] and A[n-1]
+//then for sure base of that triangle will be less than this , and hence height would be surely more than
+//min(A[0],A[n-1]) , this goes for each step
+//hence we incremenet that height which is smaller than other 
 //Hence , we increment each time the height of that side , which is less than the other
